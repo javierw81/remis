fc5d8d01fddd239ed3ca31a63e38c0f7587cfb50
Javier Wamba <javierw81@gmail.com>
2020-10-24 22:03:32 +0200

    correcciones graficas y reusabilidad

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("hola estoy entrando en el cargador de navigator");
+$(  
+    function(){
+        $('#navigator').load('views/navigator.html');
+    }
+)
+console.log("chau estoy saliendo en el cargador de navigator");
