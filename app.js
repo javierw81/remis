167bc9c9bf47deb7a3c707aa82146d9a5bf04b21
marcela quaresima <quaresimamarcela@gmail.com>
2020-10-25 09:13:24 -0300

    uso del codigo reutilizable

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ $(
     }
 )
 console.log("chau estoy saliendo en el cargador de navigator");
+
+console.log("hola estoy entrando en el cargador de navigator");
+$(  
+    function(){
+        $('#footer').load('views/footer.html');
+    }
+)
+console.log("chau estoy saliendo en el cargador de navigator");
