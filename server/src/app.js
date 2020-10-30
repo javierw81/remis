a75c411b1dccf921a6f9b4adf54efe4d46997eda
Javier Wamba <javierw81@gmail.com>
2020-10-30 16:20:36 +0100

    server API users CRUD

--- server/src/app.js
+++ server/src/app.js
@@ -7,6 +7,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const port = 3000
+const generateId= () => Math.floor(Math.random() * 10000000000)
+const db =[]
 
 app.get('/', (req, res) => {
   const ret = {
@@ -18,35 +20,55 @@ app.get('/', (req, res) => {
 })
 
 app.get('/users', (req, res) => {   
-  const ret = [
-    {
-      id: 12,
-      name: "Javier Alfredo",
-      surname: "Wamba",
-      birthdate:  new Date(1981,3,3) 
-    },
-    {
-      id: 14,
-      name: "Maria Constanza",
-      surname: "Catania",
-      birthdate:  new Date(1986,9,9) 
-    }
-  ]
+  const ret = db
+
   res.send(ret);
  })
 
  app.post('/user', (req, res) => { 
       const user=req.body 
       
-      res.send({ ...user, code: "ok"})
+      db.push({...user, id: generateId()})
+
+      res.send(user)
   })
   .put('/user/:id', (req, res) => { 
-    const id = req.params.id
+    const id = parseInt( req.params.id)
     const user=req.body 
     
-    res.send({ ...user, id, code: "ok"})
+    const index = db.findIndex(x => x.id == id) 
+    db.splice(index,1)
+
+    db.push({...user, id})
+
+    res.send(user)
+  })
+  .delete('/user/:id', (req, res) => { 
+    const id = parseInt( req.params.id)
+    const user=req.body 
+
+    const index = db.findIndex(x => x.id == id) 
+    db.splice(index,1)
+    
+    res.send({id})
+  })
+  .get('/user/:id', (req, res) => { 
+    const id = parseInt( req.params.id)
+    const user=db.find(x=>x.id==id)   
+    
+    res.send(user)
   })
 
 app.listen(port, () => {
+  db.push({
+    id:  generateId(),
+    name: "Javier Alfredo",
+    surname: "Wamba",    
+  })
+  db.push({
+    id:  generateId(),
+    name: "Maria Constanza",
+    surname: "Catania",     
+  })
   console.log(`Example app listening at http://localhost:${port}`)
 })
