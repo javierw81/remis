93f25ff3a637bf9693e84f08d4b6a45edd528f74
Javier Wamba <javierw81@gmail.com>
2020-11-17 15:12:15 +0100

    user update

--- src/users.update.js
+++ src/users.update.js
@@ -1,4 +1,4 @@
-const getUpdate = () => {
+const getDetail = () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const id = urlParams.get('id')
@@ -14,21 +14,61 @@ const getUpdate = () => {
                 const surname = document.getElementsByName('surname')[0]
                 const address = document.getElementsByName('address')[0]
                 const city = document.getElementsByName('city')[0]
-                
-                idControl.innerHTML = data.id
-                name.innerHTML = data.name
-                surname.innerHTML = data.surname
-                address.innerHTML = data.address
-                city.innerHTML = data.city
 
-
-
-
-
+                idControl.value = data.id
+                name.value = data.name
+                surname.value = data.surname
+                address.value = data.address
+                city.value = data.city
             })
     }
 }
 
+const putUser = (id, user) => {
+    if (id && user) {
+        fetch(`${api.user.default}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(user),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .catch(error => alert('Error:', error)) /* manejo de error */
+            .then(data => {
+                window.location.href = `/views/users`
+            })
+    }
+}
 
+const cancelButton = document.getElementById('cancel')
+cancelButton.onclick = () => {
+    window.location.href = `/views/users`
+}
 
-getUpdate()
+const form = document.getElementById('userForm')
+form.onsubmit = (e) => {
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+
+    const idControl = document.getElementsByName('id')[0]
+    const name = document.getElementsByName('name')[0]
+    const surname = document.getElementsByName('surname')[0]
+    const address = document.getElementsByName('address')[0]
+    const city = document.getElementsByName('city')[0]
+
+    const id = idControl.value;
+
+    const user = {
+        name: name.value,
+        surname: surname.value,
+        address: address.value,
+        city: city.value
+    }
+
+    putUser(id, user)
+
+}
+
+getDetail()
